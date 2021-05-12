4143e75d1650c4cb0da9f745e09ef12ba0adc0f1
ybuser <ybuser@naver.com>
2021-05-12 17:55:07 +0900

    Practicing commit from eclipse

---
+++ src/Frame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Frame extends JFrame {
+	public static void main(String args[]) {
+		Frame frame = new Frame();
+		
+	}
+	
+	public void init() {
+		
+	}
+	
+	public Frame() {
+		
+		
+	}
+}
