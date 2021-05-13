591b7c05754a7969b018a266f58968b45cc51759
82102 <ehdqls0212@naver.com>
2021-05-14 01:01:56 +0900

    Second Commit (55:59)

--- DB_FinalPractice/src/Screen.java
+++ DB_FinalPractice/src/Screen.java
@@ -1,21 +1,65 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.image.*;
 
 public class Screen extends JPanel implements Runnable {
 	public Thread thread = new Thread(this);
+
+	public static Image[] tileset_ground = new Image[100];
+	public static Image[] tileset_air = new Image[100];
+	
+	public static int myWidth, myHeight;
+	
+	public static boolean isFirst = true;
+	
+	public static Room room;
+
 	
 	public Screen() {
 		//setBackground(Color.PINK);
 		thread.start();
 	}
 	
-	public void paintComponent(Graphics g) {
+	public void define() {
+		room = new Room();
 		
+		for(int i = 0; i < tileset_ground.length; i++) {
+			tileset_ground[i] = new ImageIcon("res/tileset_ground.png").getImage();
+			tileset_ground[i] = createImage(new FilteredImageSource(tileset_ground[i].getSource(), new CropImageFilter(0, 26*i, 26, 26)));
+		}
+		for(int i = 0; i < tileset_air.length; i++) {
+			tileset_air[i] = new ImageIcon("res/tileset_air.png").getImage();
+			tileset_air[i] = createImage(new FilteredImageSource(tileset_air[i].getSource(), new CropImageFilter(0, 26*i, 26, 26)));
+		}
 	}
 	
+	public void paintComponent(Graphics g) {
+		if(isFirst) {
+			myWidth = getWidth();
+			myHeight = getHeight();
+			
+			define();
+			
+			isFirst = false;
+		}
+		
+		g.clearRect(0, 0, getWidth(), getHeight());
+		
+		room.draw(g); // Drawing the room
+	}
+	
+	//public static int fpsFrame = 0, fps = 1000000; 
 	public void run() {
 		while(true) {
-			System.out.println("hi");
+			if(!isFirst) {
+				
+			}
+				
+			repaint();
+			
+			try {
+				Thread.sleep(1);
+			}catch(Exception e) { }
 		}
 	}
 }
